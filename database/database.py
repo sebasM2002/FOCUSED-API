038e424232d6cfccd69a5dcceb4d75f28c248022
1102145 <sebastian.mercado1415@gmail.com>
2024-10-01 16:39:42 -0400

    Python Vercel routing pt5

--- database/database.py
+++ database/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres:Lolasa12@localhost:5432/focusedDB"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
