c7304508215cb9fdbf320a82daa733eefb96f250
1102145 <sebastian.mercado1415@gmail.com>
2024-10-01 23:59:10 -0400

    DatabaseTest

--- routers/usuario_route.py
+++ routers/usuario_route.py
@@ -18,35 +18,35 @@ async def create_users(usuario: UsuarioCreate, db: AsyncSession = Depends(get_db
     hashed_password = hash_ps((usuario.password))
     actions = "I"
     new_user = Usuario(nombre=usuario.nombre, email=usuario.email, password=hashed_password, id_rol=usuario.id_rol)
-
-    try:
-        await db.execute(
-            text("CALL p_usuario_actions(:v_id, :v_actions, :v_nombre, :v_email, :v_password, :v_role)"),
-            {
-                "v_id": new_user.id,  
-                "v_actions": actions,
-                "v_nombre": new_user.nombre,
-                "v_email": new_user.email,
-                "v_password": new_user.password,
-                "v_role": new_user.id_rol
-            }
-        )
-        await db.commit()
-        return {"message": "User created successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    async with db:
+        try:
+            await db.execute(
+                text("CALL p_usuario_actions(:v_id, :v_actions, :v_nombre, :v_email, :v_password, :v_role)"),
+                {
+                    "v_id": new_user.id,  
+                    "v_actions": actions,
+                    "v_nombre": new_user.nombre,
+                    "v_email": new_user.email,
+                    "v_password": new_user.password,
+                    "v_role": new_user.id_rol
+                }
+            )
+            await db.commit()
+            return {"message": "User created successfully"}
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
 @admin_router.get("/", summary = "Get Users")
 async def get_users(db: AsyncSession = Depends(get_db), current_user: UsuarioCreate = Depends(get_current_user)):
     if(current_user.id_rol != 0):
-        raise HTTPException( detail ="You cannot access this functionality.")
+        raise HTTPException(status_code = 400, detail ="You cannot access this functionality.")
     else:
         try:
             result = await db.execute(select(Usuario))
             admin = result.scalars().all()
             
             if not admin:
-                raise HTTPException(detail="No user found")
+                raise HTTPException(status_code = 400, detail="No user found")
             
             return admin
         except Exception as e:
@@ -55,7 +55,7 @@ async def get_users(db: AsyncSession = Depends(get_db), current_user: UsuarioCre
 @admin_router.get("/{user_id}", summary = "Get one User by ID")
 async def get_user(user_id: int, db: AsyncSession = Depends(get_db) , current_user: UsuarioCreate = Depends(get_current_user)):
     if(current_user.id_rol != 0):
-        raise HTTPException(detail ="You cannot access this functionality.")
+        raise HTTPException(status_code = 400, detail ="You cannot access this functionality.")
     else:
         try:
             result = await db.execute(
@@ -81,7 +81,7 @@ async def get_user(user_id: int, db: AsyncSession = Depends(get_db) , current_us
 @admin_router.put("/{user_id}", summary = "Update User")
 async def update_user(usuario:UsuarioEdit, db: AsyncSession = Depends(get_db) , current_user: UsuarioCreate = Depends(get_current_user)):
     if(current_user.id_rol != 0):
-        raise HTTPException(detail ="You cannot access this functionality.")
+        raise HTTPException(status_code = 400, detail ="You cannot access this functionality.")
     else:
         try:
             result = await db.execute(
@@ -122,7 +122,7 @@ async def update_user(usuario:UsuarioEdit, db: AsyncSession = Depends(get_db) , 
 @admin_router.delete("/{user_id}", summary = "Delete a user")
 async def delete_user(user: UsuarioDelete, db: AsyncSession = Depends(get_db) , current_user: UsuarioCreate = Depends(get_current_user)):
     if(current_user.id_rol != 0):
-        raise HTTPException(detail ="You cannot access this functionality.")
+        raise HTTPException(status_code = 400, detail ="You cannot access this functionality.")
     else:
         actions = "D"
         try:
@@ -155,29 +155,30 @@ async def login(user: UsuarioBase, db: AsyncSession = Depends(get_db)):
     newUser = result.fetchone()
 
     if not user:
-        raise HTTPException(detail="Invalid username or password")
+        raise HTTPException(status_code = 400, detail="Invalid username or password")
 
     hashed_password = newUser.password
 
     if not pass_verify(user.password, hashed_password):
-        raise HTTPException(detail="Invalid username or password")
+        raise HTTPException(status_code = 400, detail="Invalid username or password")
 
     return {"msg": "Login successful"}
 
 @admin_router.post("/token/", response_model = Token , summary = "JWT Token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
-    user = await authenticate_user(form_data.username, form_data.password, db)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
+    async with db:
+        user = await authenticate_user(form_data.username, form_data.password, db)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = await create_access_token(
+            data={"sub": user.nombre}, expires_delta=access_token_expires
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = await create_access_token(
-        data={"sub": user.nombre}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+        return {"access_token": access_token, "token_type": "bearer"}
 
 @admin_router.get("/me/", response_model= UsuarioCreate, summary = "Get User")
 async def read_users_me (current_user: UsuarioCreate = Depends(get_current_user)):
